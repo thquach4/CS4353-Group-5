19a68028961c601128278f92aad628ff30c41494
misyasin <98998023+misyasin@users.noreply.github.com>
2023-07-13 14:50:19 -0500

    1 file modified

--- flask-server/server.py
+++ flask-server/server.py
@@ -24,6 +24,40 @@ user_data = {
     },
 }
 
+class QuoteHistory:
+    def __init__(self):
+        self.quotes = []
+
+    def add_quote(self, gallons_requested, delivery_address, delivery_date, suggested_price, total_amount):
+        quote = {
+            'gallons_requested': gallons_requested,
+            'delivery_address': delivery_address,
+            'delivery_date': delivery_date,
+            'suggested_price': suggested_price,
+            'total_amount': total_amount
+        }
+        self.quotes.append(quote)
+
+    def get_quotes(self):
+        return self.quotes
+
+@app.route('/submit/quote', methods=['POST'])
+def submit_quote():
+    data = request.get_json()
+    gallons_requested = data.get('gallons_requested', None)
+    delivery_address = data.get('delivery_address', None)
+    delivery_date = data.get('delivery_date', None)
+    suggested_price = data.get('suggested_price', None)
+    total_amount = data.get('total_amount', None)
+
+
+    # Create an instance of QuoteHistory and add the quote
+    quote_history = QuoteHistory()
+    quote_history.add_quote(gallons_requested, delivery_address, delivery_date, suggested_price, total_amount)
+
+    # Return a response to the client
+    return jsonify({'state': 'success'})
+
 @app.route('/user/<uid>')
 def get_user_info(uid):
     if uid in user_data:
@@ -83,8 +117,8 @@ if __name__ == '__main__':
     app.run(host='127.0.0.1', port=1234, debug=True)
 
     class PricingModule:
-    def __init__(self, base_price):
-        self.base_price = base_price
+        def __init__(self, base_price):
+            self.base_price = base_price
     
     def calculate_discounted_price(self, discount):
         # TODO: Implement the calculation of discounted price
@@ -97,3 +131,22 @@ if __name__ == '__main__':
     def calculate_total_price(self, discount, tax_rate):
         # TODO: Implement the calculation of total price
         pass
+
+pass
+
+# login_module.py
+from flask import request
+
+class LoginModule:
+    @staticmethod
+    def login():
+        username = request.form['username']
+        password = request.form['password']
+        # Implement login logic here
+        # Validate credentials and return appropriate response
+        
+        # Example implementation: Check if username and password are valid
+        if username == 'admin' and password == 'password':
+            return {'message': 'Login successful'}
+        else:
+            return {'message': 'Invalid credentials'}
