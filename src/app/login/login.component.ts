213e8a71c580c7e7caf30a4c2dc6dadc222bbdad
misyasin <98998023+misyasin@users.noreply.github.com>
2023-07-14 11:28:22 -0500

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -10,44 +11,44 @@ export class LoginComponent implements OnInit, OnDestroy {
   username: string;
   password: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    // With Routing in Ionic, The OnInit lifecycle hook 
-    // may not get called consistently.
     console.log("LoginPage - OnInit");
   }
 
   ngOnDestroy() {
-    // Likewise, this will may not consistently fire when you navigate away
-    // from the component
     console.log("LoginPage - OnDestroy");
   }
-    
-  // However, Ionic provides lifecycle hooks of its own that
-  // will fire consistently during route navigation
 
   ionViewWillEnter() {
-    // This method will be called every time the component is navigated to
-    // On initialization, both ngOnInit and this method will be called
     console.log("LoginPage - ViewWillEnter");
   }
 
   ionViewWillLeave() {
-    // This method will be called every time the component is navigated away from
-    // It would be a good method to call cleanup code such as unsubscribing from observables
     console.log("LoginPage - ViewWillLeave");
   }
 
   login() {
-    // Perform login functionality
     if (this.username && this.password) {
-      // Here you can call an authentication service or perform login logic
-      console.log("Logging in...");
-      console.log("Username: ", this.username);
-      console.log("Password: ", this.password);
+      const loginData = {
+        username: this.username,
+        password: this.password
+      };
 
-      // Additional login logic such as calling an authentication service, handling responses, etc.
+      this.http.post('http://127.0.0.1:1234/login', loginData)
+        .subscribe(
+          data => {
+            console.log('Login successful.');
+            console.log(data);
+            // Handle the response from the backend if needed
+          },
+          error => {
+            console.log('Error during login.');
+            console.error(error);
+            // Handle the error if needed
+          }
+        );
     } else {
       console.log("Please provide username and password.");
     }
