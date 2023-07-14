6b6041a8abdab281fe4a16179bafb901dd4a60d6
gokce888 <136104863+gokce888@users.noreply.github.com>
2023-07-14 10:23:21 -0500

    1 file modified

--- src/app/tabs/history/history.component.ts
+++ src/app/tabs/history/history.component.ts
@@ -1,7 +1,46 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'tabs-history-page',
+  selector: 'app-history',
   templateUrl: './history.component.html',
+  
 })
-export class HistoryPageComponent {}
+export class HistoryComponent implements OnInit {
+  quoteHistory: any[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getQuoteDataFromComponent();
+  }
+
+  getQuoteDataFromComponent() {
+    // Replace the URL with your actual API endpoint to fetch quote data
+    this.http.get('https://example.com/quote-component-api')
+      .subscribe(
+        (data: any) => {
+          // Assuming the response contains the quote data as an array
+          this.quoteHistory = data;
+        },
+        (error: any) => {
+          console.error('Error fetching quote data:', error);
+        }
+      );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
