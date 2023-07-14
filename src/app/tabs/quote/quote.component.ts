1336fb05b39a150d91538b06885ac4803842b31a
misyasin <98998023+misyasin@users.noreply.github.com>
2023-07-14 11:29:57 -0500

    Update quote.component.ts

--- src/app/tabs/quote/quote.component.ts
+++ src/app/tabs/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'tabs-quote-page',
@@ -7,7 +8,33 @@ import { Component } from '@angular/core';
 export class QuotePageComponent {
   clientAddress: string;
 
+  constructor(private http: HttpClient) {}
+
   onAddressChange(event: any) {
     this.clientAddress = event.target.value;
+  }
+
+  submitQuote() {
+    const quoteData = {
+      gallons_requested: 100, // Replace with the actual value
+      delivery_address: this.clientAddress,
+      delivery_date: '2023-07-14', // Replace with the actual value
+      suggested_price: 10.0, // Replace with the actual value
+      total_amount: 1000.0, // Replace with the actual value
+    };
+
+    this.http.post('http://127.0.0.1:1234/submit/quote', quoteData)
+      .subscribe(
+        data => {
+          console.log('Quote submitted successfully.');
+          console.log(data);
+          // Handle the response from the backend if needed
+        },
+        error => {
+          console.log('Error submitting quote.');
+          console.error(error);
+          // Handle the error if needed
+        }
+      );
   }
 }
