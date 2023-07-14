1e043d9f79c808282a715bf7013a13a813a0a18a
misyasin <98998023+misyasin@users.noreply.github.com>
2023-07-14 11:29:27 -0500

    Update history.component.ts

--- src/app/tabs/history/history.component.ts
+++ src/app/tabs/history/history.component.ts
@@ -1,46 +1,35 @@
-
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
-  
 })
 export class HistoryComponent implements OnInit {
-  quoteHistory: any[] = [];
+  historyData: any[];
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.getQuoteDataFromComponent();
+    this.getHistoryData();
   }
 
-  getQuoteDataFromComponent() {
-    // Replace the URL with your actual API endpoint to fetch quote data
-    this.http.get('https://example.com/quote-component-api')
+  getHistoryData() {
+    // Replace the URL with the new endpoint for fetching quote history
+    const userId = "1000"; // Replace with the actual user ID
+    this.http.get('http://127.0.0.1:1234/quote-history/' + userId)
       .subscribe(
-        (data: any) => {
-          // Assuming the response contains the quote data as an array
-          this.quoteHistory = data;
+        (data: any[]) => {
+          console.log('Received history data.');
+          console.log(data);
+          // Store the history data or perform any required operations
+          this.historyData = data;
         },
         (error: any) => {
-          console.error('Error fetching quote data:', error);
+          console.log('Error fetching history data.');
+          console.error(error);
+          // Handle the error if needed
         }
       );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
