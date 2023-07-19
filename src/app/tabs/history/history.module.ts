ba1775d83e96868ed893462011035e1e2b23f787
gokce888 <136104863+gokce888@users.noreply.github.com>
2023-07-19 14:17:52 -0500

    1 file modified

--- src/app/tabs/history/history.module.ts
+++ src/app/tabs/history/history.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HistoryComponent } from './history.component';
-
+import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   imports: [
     IonicModule,
     RouterModule.forChild([{ path: '', component: HistoryComponent }]),
+    CommonModule,
+    HttpClientModule,
   ],
   declarations: [HistoryComponent],
   exports: [HistoryComponent],
 })
 export class HistoryPageModule {}
+
+
+
+
+
+
+
+
+
