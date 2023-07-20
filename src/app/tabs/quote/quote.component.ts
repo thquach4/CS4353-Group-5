42ac7d573fc267af2df92911453014c93f70b359
gokce888 <136104863+gokce888@users.noreply.github.com>
2023-07-20 15:28:44 -0500

    2 files modified

--- src/app/tabs/quote/quote.component.ts
+++ src/app/tabs/quote/quote.component.ts
@@ -1,26 +1,39 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { AlertController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'tabs-quote-page',
   templateUrl: './quote.component.html',
 })
 export class QuotePageComponent {
-  clientAddress: string;
+  constructor(
+    private router: Router,
+    private http: HttpClient,
+    private alertController: AlertController,
+    private route: ActivatedRoute
+  ) {}
 
-  constructor(private http: HttpClient) {}
+  delivery_address: string;
+  gallons_requested:number;
+  delivery_date:string;
+  suggested_price:number;
+  total_amount:number;
+
+  
 
   onAddressChange(event: any) {
-    this.clientAddress = event.target.value;
+    this.delivery_address = event.target.value;
   }
 
   submitQuote() {
     const quoteData = {
-      gallons_requested: 100, // Replace with the actual value
-      delivery_address: this.clientAddress,
-      delivery_date: '2023-07-14', // Replace with the actual value
-      suggested_price: 10.0, // Replace with the actual value
-      total_amount: 1000.0, // Replace with the actual value
+      gallons_requested: this.gallons_requested,
+      delivery_address: this.delivery_address,
+      delivery_date: this.delivery_date,
+      
     };
 
     this.http.post('http://127.0.0.1:1234/submit/quote', quoteData)
@@ -36,5 +49,21 @@ export class QuotePageComponent {
           // Handle the error if needed
         }
       );
+      console.log("QuotePageComponent - ionViewWillEnter")
   }
+  onSave() {
+    console.log('delivery_address:', this.delivery_address);
+    console.log('gallons_requested:', this.gallons_requested);
+    console.log('delivery_date:', this.delivery_date);
+    
+    
+    
+    const data = {
+      delivery_address:this.delivery_address,
+      gallons_requested:this.gallons_requested,
+      delivery_date:this.delivery_date,
+      
+    }
+};
+
 }
